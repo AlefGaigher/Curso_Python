efb982f38f3230575b158feb3d9a7da55943110d
Alef Garcia Gaigher <37152658+AlefGaigher@users.noreply.github.com>
2018-04-14 15:03:34 -0300

    Add files via upload

---
+++ funcoes_string.py
@@ -0,0 +1,19 @@
+texto = 'string sem PaDrao'
+
+print('capitalize => {}' .format(texto.capitalize()))
+print('title => {}' .format(texto.title()))
+print('lower => {}' .format(texto.lower()))
+print('uper => {}' .format(texto.upper()))
+print('swapcase => {}' .format(texto.swapcase()))
+print('center => <{}>' .format(texto.center(50)))
+print('center => <{}>' .format(texto.center(50,'~')))
+print('zfill => <{}>' .format(texto.zfill(50)))
+print('rjust => <{}>' .format(texto.rjust(50)))
+print('rjust => <{}>' .format(texto.rjust(50, '~')))
+print('concatenação => <{}>'.format('Concatenei'+'duas strings'))
+print('a in Alef => {}'.format('a' in 'Alef'))
+print('af in Alef => {}'.format('af' in 'Alef'))
+print('se in Alef => {}'.format('ae' in 'Alef'))
+print('se not in Alef => {}'.format('ae' not in 'Alef'))
+
+print('Multiplicação (repetição) => <{}>'.format('~' * 50))
