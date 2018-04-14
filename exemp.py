314e34f4657ee17632935d10de20720b666a1256
Alef Garcia Gaigher <37152658+AlefGaigher@users.noreply.github.com>
2018-04-14 11:12:58 -0300

    Add files via upload

---
+++ exemp.py
@@ -0,0 +1,5 @@
+def imprimir_telefone (numero_telefone, codigo_area, codigo_pais=55):
+    print('Número = {} , Codigo = {} , Pais = {}'.format(numero_telefone, codigo_area, codigo_pais))
+
+imprimir_telefone(27 , 998001322)
+imprimir_telefone(999880995, 123)
