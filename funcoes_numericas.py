e78c7834eff7907cf04c654671abbfe89d50c615
Alef Garcia Gaigher <37152658+AlefGaigher@users.noreply.github.com>
2018-04-14 14:38:21 -0300

    Add files via upload

---
+++ funcoes_numericas.py
@@ -0,0 +1,10 @@
+import math
+
+x = 1.1254
+
+print('Round => {}'.format(round(x)))
+print('Round com 2 casas decimais => {}'.format(round(x,2)))
+print('Ceil => {}'.format(math.ceil(x)))
+print('Floor => {}'.format(math.floor(x)))
+print('Trunc => {}'.format(math.trunc (x)))
+print('sqrt => {}'.format(math.sqrt (x)))
